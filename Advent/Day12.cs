0ef33b31480bbb272a4490a030d47e42391cfe47
Jay.Chalmers <jay.chalmers@redington.co.uk>
2021-12-23 14:47:19 +0000

    Day 12/13

---
+++ Advent/Day12.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Year2021
+{
+    using System;
+
+    internal class Day12 : Solution
+    {
+        private readonly Dictionary<string, List<string>> _nodes = new Dictionary<string, List<string>>();
+
+        private readonly List<List<string>> _paths = new List<List<string>>();
+
+        internal Day12()
+        {
+            foreach (var line in File.ReadAllLines(this._path))
+            {
+                var elements = line.Split('-');
+                var name = elements[0];
+                var dest = elements[1];
+                if (this._nodes.ContainsKey(name)) this._nodes[name].Add(dest);
+                else this._nodes.Add(name, new List<string> { dest });
+                if (this._nodes.ContainsKey(dest)) this._nodes[dest].Add(name);
+                else this._nodes.Add(dest, new List<string> { name });
+            }
+        }
+
+        internal void Part1()
+        {
+            foreach (var destination in this._nodes["start"])
+            {
+                this.Explore(destination, new List<string> { "start" }, part2Rules: false);
+            }
+            this.PrintPaths();
+            Display(this._paths.Count);
+        }
+
+        internal void Part2()
+        {
+            foreach (var destination in this._nodes["start"])
+            {
+                this.Explore(destination, new List<string> { "start" }, part2Rules: true);
+            }
+            Display(this._paths.Count);
+        }
+
+        private void PrintPaths()
+        {
+            foreach (var path in this._paths)
+            {
+                Console.WriteLine(string.Join(",", path));
+            }
+        }
+
+        private void Explore(string currentNode, List<string> path, bool part2Rules)
+        {
+            var currentPath = new List<string>(path) { currentNode };
+            Console.WriteLine("*****************");
+            Console.WriteLine(string.Join(",", currentPath));
+            var nodesToExplore = part2Rules ?
+                this._nodes[currentNode].Where(dest => ShouldConsiderNodePart2(dest, currentPath))
+                :
+                this._nodes[currentNode].Where(dest => ShouldConsiderNodePart1(dest, currentPath));
+            foreach (var nextNode in nodesToExplore)
+            {
+                if (nextNode == "end")
+                {
+                    var completedPath = new List<string>(currentPath) { nextNode };
+                    this._paths.Add(completedPath);
+                }
+                else
+                {
+                    Explore(nextNode, currentPath, part2Rules);
+                }
+            }
+        }
+
+        private bool ShouldConsiderNodePart1(string name, List<string> path)
+        {
+            if (IsSmallCave(name) && path.Contains(name)) return false;
+
+            return true;
+        }
+
+        private bool ShouldConsiderNodePart2(string name, List<string> path)
+        {
+            if (name == "start") return false; //start node, stop
+            if (!IsSmallCave(name)) return true; //big cave, continue
+
+            var smallCavesInPath = path.Where(stop => IsSmallCave(stop));
+            if (smallCavesInPath.Contains(name) == false)
+            {
+                //Haven't encountered this small cave before, continue
+                return true;
+            }
+            //remove unique values from small caves in path, if any remains, we have explored a small cave twice already
+            var smallCavesInPathCopy = new List<string>(smallCavesInPath);
+            foreach (var distinct in smallCavesInPath.Distinct())
+            {
+                smallCavesInPathCopy.Remove(distinct);
+            }
+
+            return smallCavesInPathCopy.Count == 0;
+        }
+
+        private List<string> GetPart2NodesToConsider()
+        {
+            return null;
+        }
+
+        private static bool IsSmallCave(string name)
+        {
+            return name.All(c => char.IsLower(c));
+        }
+    }
+}
---
+++ Advent/Day13.cs
@@ -0,0 +1,145 @@
+﻿namespace Year2021
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class Day13 : Solution
+    {
+        private readonly List<FoldInstruction> _foldInstructions = new List<FoldInstruction>();
+
+        private bool[,] _dots;
+
+        private int _width;
+
+        private int _height;
+
+        internal Day13()
+        {
+            var lines = File.ReadAllLines(this._path);
+            var dotLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    if (line.StartsWith("fold along "))
+                    {
+                        this._foldInstructions.Add(new FoldInstruction(line));
+                    }
+                    else
+                    {
+                        dotLines.Add(line);
+                    }
+                }
+            }
+
+            _width = this._foldInstructions.First(ins => ins.Orientation == 'x').Position * 2 + 1;
+            _height = this._foldInstructions.First(ins => ins.Orientation == 'y').Position * 2 + 1;
+
+            this._dots = new bool[_height, _width];
+
+            foreach (var line in dotLines)
+            {
+                var coords = line.Split(',');
+                this._dots[int.Parse(coords[1]), int.Parse(coords[0])] = true;
+            }
+        }
+
+        internal void Part1()
+        {
+            this.FoldBoard(this._foldInstructions.First());
+            this.PrintBoard();
+            Display(this.CountDots());
+        }
+
+        internal void Part2()
+        {
+            foreach (var fold in this._foldInstructions)
+            {
+                this.FoldBoard(fold);
+            }
+            this.PrintBoard();
+        }
+
+        private void FoldBoard(FoldInstruction instruction)
+        {
+            bool[,] newBoard;
+            int newHeight = this._height;
+            int newWidth = this._width;
+
+            if (instruction.Orientation == 'x')
+            {
+                newWidth = this._width / 2;
+                newBoard = new bool[this._height, newWidth];
+
+                for (int y = 0; y < this._height; y++)
+                {
+                    for (int x = 0; x < newWidth; x++)
+                    {
+                        var foldedX = this._width - 1 - x;
+                        newBoard[y, x] = this._dots[y, x] || this._dots[y, foldedX];
+                    }
+                }
+            }
+            else
+            {
+                newHeight = this._height / 2;
+                newBoard = new bool[newHeight, this._width];
+
+                for (int y = 0; y < newHeight; y++)
+                {
+                    for (int x = 0; x < this._width; x++)
+                    {
+                        var foldedY = this._height - 1 - y;
+                        newBoard[y, x] = this._dots[y, x] || this._dots[foldedY, x];
+                    }
+                }
+            }
+
+            this._height = newHeight;
+            this._width = newWidth;
+            this._dots = newBoard;
+        }
+
+        private int CountDots()
+        {
+            int count = 0;
+            foreach (var cell in this._dots)
+            {
+                if (cell) count++;
+            }
+
+            return count;
+        }
+
+        private void PrintBoard()
+        {
+            var sb = new StringBuilder();
+            for (int y = 0; y < this._height; y++)
+            {
+                for (int x = 0; x < this._width; x++)
+                {
+                    sb.Append(this._dots[y, x] ? "*" : ".");
+                }
+
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        private class FoldInstruction
+        {
+            internal char Orientation { get; }
+            internal int Position { get; }
+
+            internal FoldInstruction(string inputLine)
+            {
+                Orientation = inputLine[11];
+                Position = int.Parse(inputLine.Substring(13));
+            }
+        }
+    }
+}
